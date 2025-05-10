d129f384b4f2e168de2199769aeb6355cec58c5b
Jung Churl ju <etude139@gmail.com>
2025-05-10 23:20:21 +0900

    Create 9656.cpp

---
+++ 9656.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if(n%2==1){
+        printf("CY");
+
+    }
+    else{
+        printf("SK");
+    }
+    return 0;
+
+}
